45c4964c808673e0fe32c423661ad23cc7f0590c
Christian Serwelas <lanzuela.christian@gmail.com>
2024-12-03 06:51:01 +0800

    security

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const firebaseConfig = {
-    apiKey: "AIzaSyAcc65wZu2Dd7QO0fZvN_cPiphU67clWXw",
-    authDomain: "engkantos.firebaseapp.com",
-    projectId: "engkantos",
-    storageBucket: "engkantos.firebasestorage.app",
-    messagingSenderId: "753582774245",
-    appId: "1:753582774245:web:3c0b84c1fd234aa2edc10c",
-    measurementId: "G-J66FNN0BWZ"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
